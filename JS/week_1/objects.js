98727bec51ce8a9e9e3496b5e68d5e2898626d75
chintak <chintakmanojkumar.joshi@slu.edu>
2023-02-01 22:07:53 -0600

    adding some files and blogs

---
+++ JS/week_1/objects.js
@@ -0,0 +1,7 @@
+var person = {
+    name: "John",
+    age: 30,
+    country: "USA"
+  };
+  console.log(person.name);
+  
---
+++ JS/week_1/variables.js
@@ -0,0 +1,4 @@
+var x = 1
+var y = 12
+var sum = x + y;
+console.log(sum);
