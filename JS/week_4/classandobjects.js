85cd0baeb4ccef8ec8e19d2e2a39c5597f41d34f
chintak <chintakmanojkumar.joshi@slu.edu>
2023-02-22 22:19:52 -0600

    worked on some functions and project

---
+++ JS/week_4/classandobjects.js
@@ -0,0 +1,5 @@
+const person1 = new Person('John', 30);
+const person2 = new Person('Jane', 25);
+
+person1.sayHello(); // logs "Hello, my name is John and I am 30 years old."
+person2.sayHello(); // logs "Hello, my name is Jane and I am 25 years old."
---
+++ JS/week_4/try_catch.js
@@ -0,0 +1,5 @@
+try {
+    // some code that might throw an error
+  } catch (error) {
+    console.log('An error occurred:', error.message);
+  }
